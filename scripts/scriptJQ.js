c44565814c3f3982f100b9816184c7626d259059
Karolina Charlinska <karolina.charlinska@gmail.com>
2018-12-09 20:32:27 +0100

    Repair script and improve scrolling, delete double-touch on ios

--- scripts/scriptJQ.js
+++ scripts/scriptJQ.js
@@ -5,11 +5,32 @@ $('.menu__section a, .arrow').on('click', function () {
     $('body, html').animate({
         scrollTop: $(goToSection).offset().top - $('.menu__logo').outerHeight(true)
     }, 800)
+    $(".active").toggleClass("show")
+
 })
 
 $(".menu__burger").on("click", function () {
     $(".active").toggleClass("show");
-    $(window).one('scroll', function () {
-        $(".active").toggleClass("show");
-    });
 })
+
+if ($(".menu__burger .fa-angle-up").hasClass("show")) {
+    $(window).one('scroll', function () {
+        $(".active").toggleClass("show")
+    })
+}
+
+// $(window).one('scroll', function () {
+//     if ($(".active").hasClass("show")) {
+//         $(".active").toggleClass("show")
+//     }
+// })
+
+// The Double Tap Issue on iOS Devices
+
+$(document).ready(function () {
+    $("a").on("click touchend", function () {
+        var el = $(this);
+        var link = el.attr("href");
+        window.location = link;
+    });
+});
