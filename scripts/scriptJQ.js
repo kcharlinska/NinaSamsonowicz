9b54f6e9b6f3dfc2670581d1ccaa5c017c740544
Karolina Charlinska <karolina.charlinska@gmail.com>
2018-12-09 15:26:28 +0100

    Repair move to section links

--- scripts/scriptJQ.js
+++ scripts/scriptJQ.js
@@ -1,35 +1,11 @@
-$('#about-Nina').on('click', function () {
-    $('body, html').animate({
-        scrollTop: $('.main-content__section').position().top
-    }, 800)
-})
-
-$('#our-story').on('click', function () {
-    $('body, html').animate({
-        scrollTop: $('.story__section').position().top
-    }, 800)
-})
-
-$('#payment').on('click', function () {
-    $('body, html').animate({
-        scrollTop: $('.payment__section').position().top
-    }, 800)
-})
-
-
-$('.menu__section a').on('click', function () {
+$('.menu__section a, .arrow').on('click', function () {
     const goToSection = "[data-section=" + $(this).attr('id') + "]";
+    console.log($(goToSection).offset().top);
+    console.log($('.menu__logo').outerHeight(true));
     $('body, html').animate({
-        scrollTop: $(goToSection).position().top
-    })
+        scrollTop: $(goToSection).offset().top - $('.menu__logo').outerHeight(true)
+    }, 800)
 })
-
-// $(document).on('click', 'a', function(event){
-//     event.preventDefault();
-//     $('body').animate({
-//         scrollTop: $($.attr(this, 'href')).offset().top
-//     }, 800);
-// });
 
 $(".menu__burger").on("click", function () {
     $(".active").toggleClass("show");
