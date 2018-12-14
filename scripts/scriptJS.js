4af2dd9336375c28932bb484c4a94be98c0b1ad8
Karolina Charlinska <karolina.charlinska@gmail.com>
2018-12-14 14:00:27 +0100

    Add other languages

--- scripts/scriptJS.js
+++
@@ -1,8 +0,0 @@
-// const menuBtn = document.querySelector('.menu__burger');
-
-
-// const showMenu = () => {
-//     document.querySelectorAll('.active').forEach(item => item.classList.toggle('show'));
-// }
-
-// menuBtn.addEventListener('click', showMenu)
