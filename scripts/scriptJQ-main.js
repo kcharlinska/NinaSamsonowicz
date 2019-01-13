1cb309547fcfd7c5eb5df004cfdfa01960ac41c5
Karolina Charlinska <karolina.charlinska@gmail.com>
2019-01-13 20:32:26 +0100

    Add script to change nav item after scroll to section and clean the code

--- scripts/scriptJQ-main.js
+++ scripts/scriptJQ-main.js
@@ -1,49 +1,51 @@
-$('.menu__section a, .arrow').not('#about_hlhs--link').on('click', function () {
-    const goToSection = "[data-section=" + $(this).attr('id') + "]";
+// Smooth scroll to section after nav click
+$('.menu__section a, .arrow__section .about_Nina--link').not('.about_hlhs--link').on('click', function () {
+    const goToSection = "[data-section=" + $(this).attr('class') + "]";
     $('body, html').animate({
         scrollTop: $(goToSection).offset().top - $('.menu__logo').outerHeight(true)
     }, 800)
     toggleShowClass()
 })
 
-// function changeText() {
-//     const scrollHeight = $(document).scrollTop();
-//     if (scrollHeight < $('#about_Nina').height()) {
-//         $('#about_Nina--link').addClass("active")
-//     } else if (scrollHeight < $('#payment').height() + $('#payment').offset().top) {
-//         $('#payment--link').addClass("active")
-//     }
-// }
-// $(document).on('scroll', changeText)
-
-
-$('#about_hlhs--link, .lang_ver--link').on('click', function () {
-    toggleShowClass()
-})
-
-
-$(".menu__burger").on("click", function () {
-    toggleShowClass()
-})
-
-if ($(".menu__burger .fa-angle-up").hasClass("show")) {
-    $(window).one('scroll', function () {
-        toggleShowClass()
+//  Change Nav Item after scroll to section
+const changeMenu = () => {
+    const sectionList = document.querySelectorAll('section[data-section]:not([data-section="homepage--link"])');
+    const navHeight = document.querySelector('.menu__section').offsetHeight;
+    const scrollPosition = window.scrollY + navHeight;
+    sectionList.forEach(item => {
+        const navItem = document.querySelector(`.${item.dataset.section}`);
+        if (scrollPosition >= item.offsetTop && scrollPosition < item.offsetTop + item.offsetHeight) navItem.classList.add('active');
+        else navItem.classList.remove('active');
     })
 }
+window.addEventListener('scroll', changeMenu)
 
+//  Show/hide menu on mobile
+$(".menu__burger").on("click", function () {
+    toggleShowClass()
+});
+$('.about_hlhs--link, .lang_ver--link').on('click', function () {
+    toggleShowClass()
+})
+
+//  Menu hides after scroll
+$(window).on('scroll', function () {
+    if ($(".fa-angle-up").hasClass("show")) {
+        toggleShowClass()
+    }
+})
+
+//  Show/hide function
 function toggleShowClass() {
     $(".switch").toggleClass("show")
 }
 
-// $(window).one('scroll', function () {
-//     if ($(".active").hasClass("show")) {
-//         $(".active").toggleClass("show")
-//     }
-// })
+//  Select language on mobile
+$('.lang__select--mobi').change(function () {
+    window.location = $(this).val()
+})
 
-// The Double Tap Issue on iOS Devices
-
+//  Double Tap Issue on iOS Devices
 $(document).ready(function () {
     $("a").on("click touchend", function () {
         var el = $(this);
